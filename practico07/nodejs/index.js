c599c4e0c35da11a7dff7b37fd6931392a6628c1
oscar <camamoblamientos@gmail.com>
2022-11-08 10:11:18 -0300

    agregar tp07

---
+++ practico07/nodejs/index.js
@@ -0,0 +1,6 @@
+
+//mostrar por consola
+console.log('Hola soy un programa')
+const operacion = require("./programa")
+
+operacion.sumar(4, 3)
---
+++ practico07/nodejs/programa.js
@@ -0,0 +1,8 @@
+//sumar dos numeros con node
+function sumar(num1, num2) {
+resultado = num1 + num2;
+console.log("La suma es: "+resultado)
+}
+module.exports = {
+"sumar": sumar
+}
