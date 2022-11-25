33c384da1a499df9dfcce2193f4fdeef011dc57f
oscar <camamoblamientos@gmail.com>
2022-11-25 09:28:27 -0300

    agregue el ej2

--- practico8/ejercicio1.js
+++ practico8/ejercicio1.js
@@ -1,6 +1,11 @@
+
+'use strict'
 const fs = require('fs');
 let rawdata = fs.readFileSync('./personas.json');
+
 //console.log(JSON.stringify(rawdata)); Con esta linea me imprime en la consola el json en formato AIS
 
 let persona = JSON.parse(rawdata);
 console.log(persona);
+ // el metodo parse nos convierte el json en un objeto de javascript
+
---
+++ practico8/ejercicio2.js
@@ -0,0 +1,5 @@
+'use strict';
+let jsonData = require('./personas.json');
+//console.log(JSON.stringify(jsonData));//El método JSON.stringify() convierte un objeto o valor de JavaScript en una cadena de texto JSON
+console.log(JSON.parse(jsonData));
+
